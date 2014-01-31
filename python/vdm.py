9c270b8791b281ed8ff881cbcfaf2311fc45c9e8
Aaron Dotter <aaron.dotter@gmail.com>
2014-01-31 23:50:54 +1100

    added F-D throttle

--- python/vdm.py
+++ python/vdm.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from pylab import *
 
 '''Vehicle Dynamics Model by Rakha et al.'''
@@ -10,27 +11,11 @@ diesel_mass_density=0.754 # kg/L
 diesel_energy_density=35.86e6 # J/L
 diesel_engine_efficiency=0.5 # dimensionless
 
-#class Truck(object):
-#''' a truck '''
-#    def __init__(self):
-#        self.power=power  #engine power (W)
-#        self.trans_eff=tran_eff #transmission efficiency, dimensionless, [0-1]
-#        self.eng_eff=eng_eff
-#        self.vehicle_mass=vehicle_mass #unloaded mass of the vehicle (kg)
-#        self.load_mass=load_mass #mass of load (kg)
-#        self.fta=fta #fraction on tractive axle, dimensionless, [0-1]
-#        self.drag=drag #drag coefficient, dimensionless
-#        self.area=area #vehicle front surface area, m^2
-#        self.position=position # [x,y,z]
-#        self.fuel_tank_max=1135 # L (optional 1324 L)
-#        self.fuel_tank=0
-
-
 #tractive effort section
 #definitions
 
-P=683      #engine power, kW
-#V=20       #speed, km/h
+Pmax=6.83e5#engine power spec., W
+#V=20      #speed, km/h
 tiny=1e-3  #softening factor to avoid divide by zero
 eta=0.8    #transmission efficiency, dimensionless, [0.89-0.94] - 0.1 for accessories
 M=165000   #vehicle mass (kg)
@@ -57,19 +42,25 @@ c3 = 10   #rolling resistance coefficient
 
 grade=0. # vertical / horizontal
 
+def minmax(x,a,b): #utility
+    return max(a,min(x,b))
 
-def acceleration(V):
+def power(x): # set engine power, x in [0,1]
+    return Pmax*minmax(x,0,1)
+
+def acceleration(v,x):
     '''calculates acceleration for a truck moving at speed V'''
-    #input: V, km/h
+    #input: v, km/h, velocity
+    #input: x, [0,1], throttle
 
     #tractive effort, N
-    Ft=3600*eta*P/(V+tiny)
+    Ft=3.6*eta*power(x)/(v+tiny)
     Fmax=g*Mta*mu
     F=min(Ft,Fmax)
 
     #resistance forces
-    Ra= c1*Cd*Ch*Area*V**2      #air drag
-    Rr= g*Cr*(c2*V+c3)*M/1e3 #rolling resistance (friction)
+    Ra= c1*Cd*Ch*Area*v*v    #air drag
+    Rr= g*Cr*(c2*v+c3)*M/1e3 #rolling resistance (friction)
     Rg= g*M*grade            #grade resistance
 
     #total resistance force
@@ -78,27 +69,34 @@ def acceleration(V):
     return (F-R)/M #acceleration, m/s^2
 
 
+def drive_truck(tmax=3600,v_target=-1):
+    #input 
+    # tmax = time to stop (s)
+    # v_target = target velocity, if > 0 then adjust throttle
 
-close("all")
-fs=20
-if True:
-    #test it:
-    t =0. # time (s)
-    dt=1. # time step (s)
-    v=0.  # velocity (km/h)
+    use_throttle = v_target > 0
+
+    t =0.  # time (s)
+    dt=1.  # time step (s)
+    v=0.   # velocity (km/h)
     f=1135 # fuel (L)
+    x=1.0  # throttle [0,1] dimensionless
+    v_target = 30. # target velocity (km/h)
     fuel=[f]
     velocity=[v]
     time=[t]
+    throttle=[x]
+    q=1e-1 #sets width of interval over which throttle is adjusted
+
     #simple Euler's method ODE integration for one hour.
-    while t < 3600:
-        a=acceleration(v)   # input v in km/h, output a in m/s^2
+    while t <= tmax:
+        a=acceleration(v,x) # input v in km/h, output a in m/s^2
         #if a < 1e-4: break # stop when a gets too small.
         v+=a*dt/3.6         # update velocity, convert from m/s to km/h
         t+=dt
-        
+
         #fuel burned as energy balance 
-        output=P*1e3*dt #W*s=J
+        output=power(x)*dt #W*s=J
         input_per_L=diesel_engine_efficiency*diesel_energy_density
         fuel_burned = output/input_per_L # W / (W/L) = L
         f-=fuel_burned
@@ -106,26 +104,62 @@ if True:
         velocity.append(v)
         time.append(t)
         fuel.append(f)
-        
-    time=array(time)
-    velocity=array(velocity)
-    fuel=array(fuel)
+        throttle.append(x)
 
-    #plot velocity vs. time
-    figure(1,figsize=(8,8))
-    plot(time,velocity)
-    xticks(fontsize=fs)
-    yticks(fontsize=fs)
-    xlabel("time (s)",fontsize=fs)
-    ylabel("velocity (km/h)",fontsize=fs)
-    
-    figure(2,figsize=(8,8))
-    plot(time,fuel/fuel[0])
-    xticks(fontsize=fs)
-    yticks(fontsize=fs)
-    xlabel("time (s)",fontsize=fs)
-    ylabel("fuel (L)",fontsize=fs)
-    show()
+        # adjust throttle by comparing v with v_target
+        # throttle control model is Fermi-Dirac with
+        # parameter q to control the width of the interval
+        if use_throttle: x=1./(exp((v-v_target)/q)+1.)
+
+    return array(time),array(velocity),array(fuel),array(throttle)
+
+close("all")
+fs=20
+
+#do one full throttle
+tmax=1200 #(s)
+t1,v1,f1,thr1=drive_truck(tmax)
+
+#do one with a target velocity
+v_target=30 #kph
+t2,v2,f2,thr2=drive_truck(tmax,v_target)
+
+xmin=0
+xmax=tmax
+#plot velocity vs. time
+fig1=figure(1,figsize=(10,10))
+fig1.subplots_adjust(top=0.96,right=0.96)
+subplot(311)
+plot(t1,v1,color="Blue",label="full")
+plot(t2,v2,color="Red",label="adjust")
+xticks(fontsize=fs)
+yticks(fontsize=fs)
+#xlabel("time (s)",fontsize=fs)
+ylabel("velocity (km/h)",fontsize=fs)
+legend(loc='lower right',fontsize=fs)
+xlim(xmin,xmax)
+
+#figure(2,figsize=(8,8))
+subplot(312)
+plot(t1,f1,color="Blue")
+plot(t2,f2,color="Red")
+xticks(fontsize=fs)
+yticks(fontsize=fs)
+#xlabel("time (s)",fontsize=fs)
+ylabel("fuel (L)",fontsize=fs)
+xlim(xmin,xmax)
+
+#figure(3,figsize=(8,8))
+subplot(313)
+plot(t1,thr1,color="Blue")
+plot(t2,thr2,color="Red")
+xticks(fontsize=fs)
+yticks(fontsize=fs)
+xlabel("time (s)",fontsize=fs)
+ylabel("throttle",fontsize=fs)
+xlim(xmin,xmax)
+ylim(0.8,1.01)
+show()
 
 if False:
     masses=linspace(75000,165000,10)
